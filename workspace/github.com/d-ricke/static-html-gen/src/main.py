d180becb44f7f807d13059297a97080b3a0a0966
d-ricke <15657574+d-ricke@users.noreply.github.com>
2025-05-29 09:52:44 -0600

    Built TextNode

---
+++ workspace/github.com/d-ricke/static-html-gen/src/main.py
@@ -0,0 +1,11 @@
+#./main.sh
+#hello world
+
+from textnode import *
+
+def main():
+    test_textnode = TextNode("Tomato", TextType.NORMAL_TEXT, "tofu.us")
+    
+    print(test_textnode)
+
+main()
---
+++ workspace/github.com/d-ricke/static-html-gen/src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = ""
+    BOLD_TEXT = "**"
+    ITALIC_TEXT = "_"
+    CODE_TEXT = "`"
+    LINKS = f"[anchor](url)"
+    IMAGES = f"![alt](url)"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, value):
+        if self.text == value.text and self.text_type == value.text_type and self.url == value.url:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
