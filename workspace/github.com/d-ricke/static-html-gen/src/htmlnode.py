e1d022e0580687a123e1dd5789701c9dc1618ae3
d-ricke <15657574+d-ricke@users.noreply.github.com>
2025-06-01 17:22:20 -0600

    Fixed HTMLNode

--- workspace/github.com/d-ricke/static-html-gen/src/htmlnode.py
+++ workspace/github.com/d-ricke/static-html-gen/src/htmlnode.py
@@ -23,4 +23,14 @@ class HTMLNode():
 
 class LeafNode (HTMLNode):
     def __init__ (self, tag: str, value: str, props: dict | None=None):
-        super(Child, self).__init__()
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise Exception(ValueError)
+        
+        elif self.tag == None:
+            return (self.value)
+        
+        else:
+            return (f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>')
--- workspace/github.com/d-ricke/static-html-gen/src/test_htmlnode.py
+++ workspace/github.com/d-ricke/static-html-gen/src/test_htmlnode.py
@@ -16,7 +16,7 @@
 
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -38,6 +38,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+        self.assertEqual(node, '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
 
 
 if __name__ == "__main__":
